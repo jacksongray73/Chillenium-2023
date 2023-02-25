818f3a7e7d27ed8a161335505d6b3a5a564118c6
Jackson Gray <jackson@Jacksons-Mac-mini.local>
2023-02-25 14:48:22 -0600

    Update Interactable.cs

--- Chillenium 2023/Assets/Scripts/Interactable.cs
+++ Chillenium 2023/Assets/Scripts/Interactable.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 public class Interactable : MonoBehaviour {
     [SerializeField] string interactTag;
     [SerializeField] GameObject _speechBubble;
+    private float _speechBubbleHeight, _speechBubbleSpeed;
+    private bool _showingBubble;
     private GameObject _currentInteraction;
+    void Start() {
+        _speechBubbleHeight = 1;
+        _speechBubbleSpeed = 10;
+    }
+
     void Update() {
-        
+        if (_showingBubble) {
+            Vector3 bubblePos = _speechBubble.transform.position;
+            Vector3 position = transform.position;
+            if (bubblePos.y < transform.position.y + _speechBubbleHeight) {
+                bubblePos.y += _speechBubbleSpeed * Time.deltaTime;
+                if (bubblePos.y > position.y + _speechBubbleHeight) {
+                    bubblePos.y = position.y + _speechBubbleHeight;
+                }
+                _speechBubble.transform.position = bubblePos;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
@@ -15,8 +32,9 @@ public class Interactable : MonoBehaviour {
             _currentInteraction = collision.gameObject;
             //Pop up speech bubble
             Vector3 position = transform.position;
-            position.y += 1;
+            position.y += 0;
             _speechBubble.transform.position = position;
+            _showingBubble = true;
         }
     }
 
@@ -26,6 +44,7 @@ public class Interactable : MonoBehaviour {
                 _currentInteraction = null;
                 //Remove speech bubble (moves it offscreen)
                 _speechBubble.transform.position = new Vector3(-10, -10, 0);
+                _showingBubble = false;
             }
         }
     }
